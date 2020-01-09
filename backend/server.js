ad91a0e8edc6ea1e52fdeeb520509367bd95ca55
ductoanthanh <arsenaltoan10@gmail.com>
2020-01-09 15:20:22 +0200

    Connect to socket & get all food items from MongoDB collection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const db = require("monk")(process.env.DB_URL);
@@ -8,9 +9,29 @@ const db = require("monk")(process.env.DB_URL);
 const collection_foodItems = db.get("FoodItems"); // collection on MongoDB
 
 const app = express();
+app.use(bodyParser.json())
 
 // our server instance
 const server = http.createServer(app);
+
+// This creates our socket using the instance of the server
+const io = socketIO(server);
+
+io.on("connection", socket => {
+  console.log("New client connected" + socket.id);
+
+  // Returning the initial data of food menu from FoodItems collection
+  socket.on("initial_data", () => {
+    collection_foodItems.find({}).then(docs => {
+      io.sockets.emit("get_data", docs);
+    });
+  });
+
+  // disconnect is fired when a client leaves the server
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 const port = process.env.PORT || 3001;
 
