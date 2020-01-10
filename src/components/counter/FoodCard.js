053ef7262e0372eea4200a8fce388634910142de
ductoanthanh <arsenaltoan10@gmail.com>
2020-01-10 22:28:38 +0200

    Quick refactor

--- src/components/counter/FoodCard.js
+++ src/components/counter/FoodCard.js
@@ -1,40 +1,40 @@
-import React, { Component } from "react";
+import React from "react";
+import { VibamiConsumer } from "../../context/context";
 
-class FoodCard extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+export const FoodCard = props => {
+  return (
+    <VibamiConsumer>
+      {value => {
+        const { cart, modifyCart } = value;
+        const food = props.food;
 
-  addToCart = food => {
-    let id = food._id;
-    let currentCart = { ...this.props.cart };
-    currentCart[id] = {
-      quantity: currentCart[id] ? currentCart[id].quantity + 1 : 1,
-      name: food.name
-    };
-    this.props.modifyCart(currentCart);
-  };
+        const addToCart = food => {
+          let id = food._id;
+          let currentCart = { ...cart };
+          currentCart[id] = {
+            quantity: currentCart[id] ? currentCart[id].quantity + 1 : 1,
+            name: food.name
+          };
+          modifyCart(currentCart);
+        };
 
-  render() {
-    const food = this.props.food;
-    return (
-      <div className={this.props.colNum}>
-        <div className="card bwm-card">
-          <img
-            className="card-img-top"
-            src="https://d2vwsr3mua7yp8.cloudfront.net/aa1bcb8f-90c1-4ea1-9ff0-039be063b587_d3.jpg" // needs to be changed
-            alt={food.name}
-          ></img>
-          <div className="card-block">
-            <h4 className="card-title">{food.name}</h4>
-            <p className="card-text">{food._id}</p>
-            <button onClick={() => this.addToCart(food)}>Add to Cart</button>
+        return (
+          <div className={props.colNum}>
+            <div className="card bwm-card">
+              <img
+                className="card-img-top"
+                src="https://d2vwsr3mua7yp8.cloudfront.net/aa1bcb8f-90c1-4ea1-9ff0-039be063b587_d3.jpg" // needs to be changed
+                alt={food.name}
+              ></img>
+              <div className="card-block">
+                <h4 className="card-title">{food.name}</h4>
+                <p className="card-text">{food._id}</p>
+                <button onClick={() => addToCart(food)}>Add to Cart</button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default FoodCard;
+        );
+      }}
+    </VibamiConsumer>
+  );
+};
--- src/components/counter/FoodList.js
+++ src/components/counter/FoodList.js
@@ -1,19 +1,10 @@
 import React, { Component } from "react";
-import FoodCard from "./FoodCard";
+import { FoodCard } from "./FoodCard";
 
 export class FoodList extends Component {
   renderFoods() {
-    //console.log(this.props);
     return this.props.foods.map((food, index) => {
-      return (
-        <FoodCard
-          key={index}
-          modifyCart={this.props.modifyCart}
-          cart={this.props.cart}
-          colNum="col-md-3 col-xs-6"
-          food={food}
-        />
-      );
+      return <FoodCard key={index} colNum="col-md-3 col-xs-6" food={food} />;
     });
   }
 
