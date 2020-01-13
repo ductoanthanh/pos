c00a6b75fb40d4b7e15988d0e368cac40c45ec6b
ductoanthanh <arsenaltoan10@gmail.com>
2020-01-13 16:54:53 +0200

    Change css --> scss

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./App.scss";
 import { Header } from "./global/header";
 import { Switch, Route } from "react-router-dom";
 import OrderListing from "./components/order/order-listing/OrderListing";
-import Counter from "./components/counter/Counter";
+import Counter from "./pages/counter/Counter";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Switch>
-          <Route exact path="/" component={Counter} />
-          <Route path="/kitchen" component={OrderListing} />
-        </Switch>
+        <div className="main-pane">
+          <Switch>
+            <Route exact path="/" component={Counter} />
+            <Route path="/kitchen" component={OrderListing} />
+          </Switch>
+        </div>
       </div>
     );
   }
