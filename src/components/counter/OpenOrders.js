5c5223207e3040ce5bda05cf81f4113dc9148abe
Duc Toan Thanh <toanth@metropolia.fi>
2020-01-14 14:47:41 +0200

    Open orders with correct statuses & items

--- src/components/counter/OpenOrders.js
+++ src/components/counter/OpenOrders.js
@@ -1,47 +1,96 @@
-import React from "react";
+import React, { Component } from "react";
 import { Cart } from "./Cart";
 import "./OpenOrders.scss";
+import { socket } from "../../global/header";
 
-export const OpenOrders = () => {
-  return (
-    <div>
-      <div className="order-manage__heading">
-        <p className="margin0">
-          No. of orders: <strong>15</strong>
-        </p>
-        <button className="primary-btn">New order</button>
+export class OpenOrders extends Component {
+  constructor() {
+    super();
+    this.state = {
+      orderData: [] // connect to sockets,
+    };
+  }
+
+  getOrdersData = orders => {
+    this.setState({ orderData: orders });
+  };
+
+  changeData = () => socket.emit("get_orders");
+
+  componentDidMount() {
+    socket.emit("get_orders");
+    socket.on("get_order_data", this.getOrdersData);
+    socket.on("change_data", this.changeData);
+  }
+
+  componentWillUnmount() {
+    socket.off("get_order_data");
+    socket.off("change_data");
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="order-manage__heading">
+          <p className="margin0">
+            No. of orders: <strong>{this.state.orderData.length}</strong>
+          </p>
+          <button className="primary-btn">New order</button>
+        </div>
+        {this.state.orderData.map((order, index) => {
+          console.log(order.foods);
+          return (
+            <div key={index} className="order-manage__card">
+              <div className="order-manage__card-header">
+                <h5 className="margin0">{order.title}</h5>
+                <span>${order.totalPrice}</span>
+              </div>
+              <div className="order-manage__card-status-list">
+                <span
+                  className={
+                    order.isDone
+                      ? "order-manage__card-status-ready"
+                      : "order-manage__card-status-in-progress"
+                  }
+                >
+                  {order.isDone ? "Ready" : "In Progress"}
+                </span>
+                <span
+                  className={
+                    order.paid
+                      ? "order-manage__card-status-unpaid"
+                      : "order-manage__card-status-paid"
+                  }
+                >
+                  {order.paid ? "Paid" : "Unpaid"}
+                </span>
+              </div>
+              <p className="margin0">
+                Dishes:&nbsp;
+                <strong>
+                  {
+                    order.foods.filter(food =>
+                      ["main", "appertizer"].includes(food.category)
+                    ).length
+                  }
+                  &nbsp;items
+                </strong>
+                <br />
+                Drinks &amp; desserts:&nbsp;
+                <strong>
+                  {
+                    order.foods.filter(food =>
+                      ["drink", "dessert"].includes(food.category)
+                    ).length
+                  }
+                  &nbsp;items
+                </strong>
+              </p>
+            </div>
+          );
+        })}
+        <Cart />
       </div>
-      <div className="order-manage__card">
-        <div className="order-manage__card-header">
-          <h5 className="margin0">Table 1</h5>
-          <span>$140</span>
-        </div>
-        <div className="order-manage__card-status-list">
-          <span className={`order-manage__card-status-ready`}>Ready</span>
-          <span className={`order-manage__card-status-unpaid`}>Unpaid</span>
-        </div>
-        <p className="margin0">
-          Dishes: <strong>4 items</strong> <br />
-          Drinks &amp; desserts: <strong>5 items</strong>
-        </p>
-      </div>
-      <div className="order-manage__card">
-        <div className="order-manage__card-header">
-          <h5 className="margin0">Table 2</h5>
-          <span>$140</span>
-        </div>
-        <div className="order-manage__card-status-list">
-          <span className={`order-manage__card-status-in-progress`}>
-            In Progress
-          </span>
-          <span className={`order-manage__card-status-paid`}>Paid</span>
-        </div>
-        <p className="margin0">
-          Dishes: <strong>4 items</strong> <br />
-          Drinks & desserts: <strong>5 items</strong>
-        </p>
-      </div>
-      <Cart />
-    </div>
-  );
-};
+    );
+  }
+}
