07a2ecb2a279d4772c3b7fbb98149750a9608c06
ductoanthanh <arsenaltoan10@gmail.com>
2020-01-14 21:25:24 +0200

    Order logic + styling

--- src/components/counter/OpenOrders.js
+++ src/components/counter/OpenOrders.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import { Cart } from "./Cart";
 import { Order } from "./Order";
+import { OrderStatus } from "./OrderStatus";
 import "./OpenOrders.scss";
 import { socket } from "../../global/header";
+import { isFood } from "../../helpers";
+import { VibamiConsumer } from "../../context/context";
 
 export class OpenOrders extends Component {
   constructor() {
     super();
     this.state = {
       orders: [], // connect to sockets,
-      isOrderOpen: false,
       selectedOrder: {}
     };
   }
@@ -32,75 +34,65 @@ export class OpenOrders extends Component {
   }
 
   render() {
-    console.log(this.state.orders);
     return (
-      <div>
-        <div className="order-manage__heading">
-          <p className="margin0">
-            No. of orders: <strong>{this.state.orders.length}</strong>
-          </p>
-          <button className="primary-btn">New order</button>
-        </div>
-        {/* render all open orders */
-        this.state.orders.map((order, index) => {
-          let foodQty = 0;
-          const totalQty = order.foods.reduce((accumulator, currentValue) => {
-            if (
-              ["main", "appertizer"].includes(currentValue.itemInfo.category)
-            ) {
-              foodQty += currentValue.quantity; // food & appertizer quantity
-            }
-            return accumulator + currentValue.quantity;
-          }, 0); // all items quantity
+      <VibamiConsumer>
+        {value => {
+          const {
+            sideView,
+            setSideView,
+            selectedOrder,
+            setSelectedOrder
+          } = value;
           return (
-            <div
-              key={index}
-              className="order-manage__card"
-              onClick={() =>
-                this.setState({ isOrderOpen: true, selectedOrder: order })
-              }
-            >
-              <div className="order-manage__card-header">
-                <h5 className="margin0">{order.title}</h5>
-                <span>${order.totalPrice}</span>
+            <div>
+              <div className="order-manage__heading">
+                <p className="margin0">
+                  No. of orders: <strong>{this.state.orders.length}</strong>
+                </p>
+                <button className="primary-btn">New order</button>
               </div>
-              <div className="order-manage__card-status-list">
-                <span
-                  className={
-                    order.isDone
-                      ? "order-manage__card-status-ready"
-                      : "order-manage__card-status-in-progress"
-                  }
-                >
-                  {order.isDone ? "Ready" : "In Progress"}
-                </span>
-                <span
-                  className={
-                    order.paid
-                      ? "order-manage__card-status-unpaid"
-                      : "order-manage__card-status-paid"
-                  }
-                >
-                  {order.paid ? "Paid" : "Unpaid"}
-                </span>
-              </div>
-              <p className="margin0">
-                Dishes:&nbsp;
-                <strong>{foodQty} &nbsp;items</strong>
-                <br />
-                Drinks &amp; desserts:&nbsp;
-                <strong>{totalQty - foodQty} &nbsp;items</strong>
-              </p>
+              {/* render all open orders */
+              this.state.orders.map((order, index) => {
+                let foodQty = 0;
+                const totalQty = order.foods.reduce(
+                  (accumulator, currentValue) => {
+                    if (isFood(currentValue.itemInfo.category)) {
+                      foodQty += currentValue.quantity; // food & appertizer quantity
+                    }
+                    return accumulator + currentValue.quantity;
+                  },
+                  0
+                ); // all items quantity
+                return (
+                  <div
+                    key={index}
+                    className="order-manage__card"
+                    onClick={() => {
+                      this.setState({ selectedOrder: order });
+                      setSideView("order-manage");
+                      setSelectedOrder(order);
+                    }}
+                  >
+                    <div className="order-manage__card-header">
+                      <h5 className="margin0">{order.title}</h5>
+                      <span>${order.totalPrice}</span>
+                    </div>
+                    <OrderStatus order={order} />
+                    <p className="margin0">
+                      Dishes:&nbsp;
+                      <strong>{foodQty} &nbsp;items</strong>
+                      <br />
+                      Drinks &amp; desserts:&nbsp;
+                      <strong>{totalQty - foodQty} &nbsp;items</strong>
+                    </p>
+                  </div>
+                );
+              })}
+              <Cart />
             </div>
           );
-        })}
-        {this.state.isOrderOpen ? (
-          <Order order={this.state.selectedOrder} />
-        ) : (
-          ""
-        )}
-        <Cart />
-      </div>
+        }}
+      </VibamiConsumer>
     );
   }
 }
