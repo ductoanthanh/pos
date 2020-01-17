2dcf70421c32d9f874f311c22ddbe85038e55dc7
Duc Toan Thanh <toanth@metropolia.fi>
2020-01-17 11:33:22 +0200

    Refactor OrderListing & OrderCard from class component to function with hooks

--- src/components/order/order-listing/OrderCard.js
+++ src/components/order/order-listing/OrderCard.js
@@ -1,56 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { socket } from "../../../global/header";
 import { Timer } from "./Timer";
 import SwipeButton from "../../../global/SwipeButton";
 
-class OrderCard extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isDone: false
-    };
-  }
+export const OrderCard = props => {
+  const { order, colNum, index } = props;
+  const [isDone, setIsDone] = useState(false);
 
-  markDone = id => {
-    console.log(id);
-    this.setState({ isDone: true });
+  const markDone = id => {
+    setTimeout(() => {
+      setIsDone(true);
+    }, 1000);
     socket.emit("mark_order_done", id);
   };
 
-  render() {
-    const order = this.props.order;
-    return (
-      <div
-        className={this.props.colNum}
-        style={{ display: this.state.isDone ? "none" : "block" }}
-      >
-        <Timer createdAt={order.createdAt} />
-        <div className="card">
-          <div className="card-block">
-            <h4 className="card-title">{order.title}</h4>
-            <p className="card-text">Guests: {order.guests}</p>
-            {order.foods.map((food, index) => {
-              return (
-                <p key={index}>
-                  {food.quantity}x {food.itemInfo.name} {food.variant.title}
-                  <br />
-                  <span style={{ color: "#C4CDD5" }}>
-                    {food.additionalInfo}
-                  </span>
-                </p>
-              );
-            })}
-            <SwipeButton
-              index={this.props.index}
-              text="Food Ready"
-              color="#009de0"
-              onSuccess={() => this.markDone(order._id)}
-            />
-          </div>
+  return (
+    <div className={colNum} style={{ display: isDone ? "none" : "block" }}>
+      <Timer createdAt={order.createdAt} />
+      <div className="card">
+        <div className="card-block">
+          <h4 className="card-title">{order.title}</h4>
+          <p className="card-text">Guests: {order.guests}</p>
+          {order.foods.map((food, index) => {
+            return (
+              <p key={index}>
+                {food.quantity}x {food.itemInfo.name} {food.variant.title}
+                <br />
+                <span style={{ color: "#C4CDD5" }}>{food.additionalInfo}</span>
+              </p>
+            );
+          })}
+          <SwipeButton
+            index={index}
+            text="Food Ready"
+            color="#009de0"
+            onSuccess={() => markDone(order._id)}
+          />
         </div>
       </div>
-    );
-  }
-}
-
-export default OrderCard;
+    </div>
+  );
+};
