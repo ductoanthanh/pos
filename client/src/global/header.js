268abaaaea2f9d43853c0f463ed8839d01f6fbd2
Duc Toan Thanh <toanth@metropolia.fi>
2020-01-21 16:41:50 +0200

    Modify Docker config

--- client/src/global/header.js
+++ client/src/global/header.js
@@ -3,38 +3,25 @@ import { NavLink } from "react-router-dom";
 import socketIOClient from "socket.io-client";
 import "./header.scss";
 
-// The Header creates links that can be used to navigate
-// between routes.
-let socket;
-class Header extends Component {
-  constructor() {
-    super();
-    this.state = {
-      endpoint: `${window.location.hostname}` // Update 5000 with port on which backend is running.
-    };
+const socket = socketIOClient("/");
 
-    socket = socketIOClient(this.state.endpoint);
-  }
-
-  render() {
-    console.log("hiiiiiiiiii");
-    return (
-      <header>
-        <nav>
-          <ul className="NavClass">
-            <li>
-              <NavLink exact to="/">
-                Counter
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/kitchen"> Kitchen</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul className="NavClass">
+          <li>
+            <NavLink exact to="/">
+              Counter
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/kitchen"> Kitchen</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export { Header, socket };
