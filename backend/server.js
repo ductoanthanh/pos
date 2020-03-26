46f94673428c96eaa538d11d0d16c791f3c4f0bf
ductoanthanh <arsenaltoan10@gmail.com>
2020-03-26 17:00:18 +0200

    Clear socket

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const http = require("http");
-const socketIO = require("socket.io");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 let cors = require("cors");
@@ -14,105 +13,12 @@ const keys = require("./keys");
 
 const db = require("monk")(keys.DB_URL);
 
-const Order = require("./models/order");
-const Food = require("./models/food");
-
-const collection_foodItems = db.get("FoodItems"); // collection on MongoDB
-const collection_orders = db.get("orders"); // collection on MongoDB
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 // our server instance
 const server = http.createServer(app);
-const io = socketIO(server); // instances of io
-
-io.on("connection", socket => {
-  console.log("New client connected" + socket.id);
-
-  // Returning the initial data of food menu from FoodItems collection
-  socket.on("initial_data", () => {
-    collection_foodItems.find({}).then(docs => {
-      io.emit("get_data", docs);
-    });
-  });
-
-  // Functionality to change the predicted quantity value
-  socket.on("ChangePred", predicted_data => {
-    collection_foodItems
-      .update(
-        { _id: predicted_data._id },
-        { $set: { predQty: predicted_data.predQty } }
-      )
-      .then(updatedDoc => {
-        // Socket event to update the Predicted quantity across the Kitchen
-        io.emit("change_data");
-      });
-  });
-
-  // Returning the initial data of orders from Orders collection
-  socket.on("get_orders", () => {
-    Order.find({ isDone: false })
-      .populate("foods.itemInfo")
-      .populate("foods.variant")
-      .then(docs => {
-        io.emit("get_order_data", docs);
-      });
-  });
-
-  // Returning the initial data of orders from Orders collection
-  socket.on("get_foods", () => {
-    Food.find()
-      .populate("variants")
-      .then(docs => {
-        io.emit("get_food_data", docs);
-      });
-  });
-
-  // Returning the initial data of orders from Orders collection
-  socket.on("add_order", request => {
-    const { title, totalPrice, guests, foods } = request;
-
-    const order = new Order({ title, totalPrice, guests, foods });
-
-    Order.create(order, (err, newOrder) => {
-      if (err) {
-        console.log(err);
-      }
-
-      foods.forEach(food => {
-        Food.findById(food.itemInfo)
-          .populate("orders")
-          .exec((err, foundFood) => {
-            if (err) {
-              console.log(err);
-            }
-
-            foundFood.orders.push(newOrder._id);
-            foundFood.save();
-          });
-      });
-
-      io.emit("change_data");
-    });
-  });
-
-  // Order completion
-  socket.on("mark_order_done", id => {
-    collection_orders
-      .update({ _id: id }, { $set: { isDone: true } })
-      .then(updatedDoc => {
-        // Updating the different Kitchen area with the current Status.
-        io.emit("change_order_data");
-      });
-  });
-
-  // disconnect is fired when a client leaves the server
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
 
 // connect database for endpoints
 mongoose
@@ -132,10 +38,6 @@ app.use("/api/v1/variants", variantRoutes);
 
 app.get("/api/", (req, res, next) => {
   res.send("Hi from api backend");
-});
-
-app.get("/", (req, res) => {
-  res.send("Hi from backend!");
 });
 
 const port = process.env.PORT || 5000;
